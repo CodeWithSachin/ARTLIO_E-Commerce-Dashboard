8aaf46bb1bdf01ae72971eea92d8847340bc885a
CodeWithSachin <sachinnnsingh0510@gmail.com>
2022-03-09 14:45:32 +0530

    first commit

---
+++ order.js
@@ -0,0 +1,50 @@
+const Orders=[
+    {
+        productName:'Foldable mini drone',
+        productNumber:'85641',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Non-Foldable mini drone',
+        productNumber:'85642',
+        paymentStatus:'done',
+        shipping:'Shipped'
+    },
+    {
+        productName:'Mini drone',
+        productNumber:'85643',
+        paymentStatus:'declined',
+        shipping:'Pending'
+    },
+    {
+        productName:'Foldable mini drone',
+        productNumber:'85641',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Foldable mini drone',
+        productNumber:'85641',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Foldable mini drone',
+        productNumber:'85641',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Foldable mini drone',
+        productNumber:'85641',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Foldable mini drone',
+        productNumber:'85641',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+]
